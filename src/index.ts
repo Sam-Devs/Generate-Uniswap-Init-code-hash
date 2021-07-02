4f815e1975cc5d1c24f135fc68ce5e54d96dba50
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-02 21:03:06 +0100

    compiled

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ const token1 = "0x2706791DbBb09b4449Cc7A79b4fF39efAFfb8c82";
 
 const pair = getCreate2Address(FACTORY_ADDRESS, keccak256(["bytes"], [pack(["address", "address"], [token0, token1])]), INIT_CODE_HASH);
 
-
-
 export const logPair = () => {
     console.log(pair);
 }
