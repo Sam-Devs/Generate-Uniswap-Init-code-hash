a5ffea97a08687c81d82c7da21f0e288234b92b8
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-02 20:48:31 +0100

    index.ts file updated

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { FACTORY_ADDRESS, INIT_CODE_HASH } from "@uniswap/sdk";
-import { getCreate2Address } from "@ethersproject/address";
-import { pack, keccak256 } from "@ethersproject/solidity";
-const token0 = "0x309138aCA968Dce99FF1677eE1672d9187b57cCa";
-const token1 = "0x2706791DbBb09b4449Cc7A79b4fF39efAFfb8c82";
-const pair = getCreate2Address(FACTORY_ADDRESS, keccak256(["bytes"], [pack(["address", "address"], [token0, token1])]), INIT_CODE_HASH);
-console.log(pair);
-//# sourceMappingURL=index.js.map
